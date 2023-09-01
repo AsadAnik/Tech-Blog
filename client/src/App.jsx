650a7e9d944b9fb209cbbd284a2176a2887399b6
devilarmaan <devilarmaan63@gmail.com>
2023-09-02 00:07:26 +0600

    initial status

---
+++ client/src/App.jsx
@@ -0,0 +1,40 @@
+// import React from "react";
+// import { Redirect, Route, Switch } from "react-router-dom";
+// import "./App.css";
+// import Blog from "./pages/Blog";
+// import Home from "./pages/Home";
+
+// const App = () => {
+//   return (
+//     <div className="container">
+//       <Switch>
+//         <Route path="/" exact component={Home} />
+//         <Route path="/blog/:id" component={Blog} />
+//         <Redirect to="/" />
+//       </Switch>
+//     </div>
+//   );
+// };
+
+// export default App;
+
+import React from "react";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import "./App.css";
+import Blog from "./pages/Blog";
+import Home from "./pages/Home";
+
+const App = () => {
+  return (
+    <div className="container">
+      <Router>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/blog/:id" element={<Blog />} />
+        </Routes>
+      </Router>
+    </div>
+  );
+};
+
+export default App;
---
+++ client/src/components/Home/Header/index.jsx
@@ -0,0 +1,53 @@
+// import React from "react";
+// import "./styles.css";
+
+// const Header = () => (
+//   <header className="home-header">
+//     <nav>
+//       <div>
+//         <ul>
+//           <li><a href="#">Home</a></li>
+//           <li><a href="#">About</a></li>
+//           <li><a href="#">Help</a></li>
+//         </ul>
+//       </div>
+//     </nav>
+//     <h1>
+//       <span>“</span> Blog <span>”</span>
+//     </h1>
+//     <p>
+//       awesome place to make oneself <br /> productive and entertained through
+//       daily updates.
+//     </p>
+//   </header>
+// );
+
+// export default Header;
+
+import React from "react";
+import "./styles.css";
+
+const Header = () => (
+  <header className="home-header">
+    <nav className="top-navbar">
+      <ul>
+        <li><a href="#">Home</a></li>
+        <li><a href="#">About</a></li>
+        <li><a href="#">Help</a></li>
+      </ul>
+    </nav>
+    <h1>
+      <span>“</span> Blog <span>”</span>
+    </h1>
+    <div className="category-box">
+      <ul>
+        <li><a href="#">Category 1</a></li>
+        <li><a href="#">Category 2</a></li>
+        <li><a href="#">Category 3</a></li>
+        {/* Add more categories as needed */}
+      </ul>
+    </div>
+  </header>
+);
+
+export default Header;
