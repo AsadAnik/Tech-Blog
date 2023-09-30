21905da29bca59e873769fee7ea1413dec4faedf
devilarmaan <devilarmaan63@gmail.com>
2023-10-01 01:07:09 +0600

    SPRINT-1: Redux implementation done

--- app/providers.jsx
+++ app/providers.jsx
@@ -1,10 +1,14 @@
 'use client'
 import { NextUIProvider } from '@nextui-org/react';
+import { Provider } from 'react-redux';
+import store from '@/redux/store';
 
 export function Providers({ children }) {
     return (
-        <NextUIProvider>
-            {children}
-        </NextUIProvider>
+        <Provider store={store}>
+            <NextUIProvider>
+                {children}
+            </NextUIProvider>
+        </Provider>
     )
 }
